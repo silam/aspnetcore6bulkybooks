93617ee906039b867ac83fedccae540cef50d095
Si Lam <silam@hotmail.com>
2021-11-28 10:40:21 -0600

    Task wait until other task completed

--- TestAsync/TestAsync/Program.cs
+++ TestAsync/TestAsync/Program.cs
@@ -4,36 +4,45 @@ using System.Threading.Tasks;
 namespace TestAsync
 {
     class Program
-    {
-        static void Main(string[] args)
-        {
-            Method1();
-            Method2();
-            Console.ReadKey();
-        }
+ {
+   static async Task Main(string[] args)
+   {
+     await callMethod();
+     Console.ReadKey();
+   }
 
-        public static async Task Method1()
-        {
-            await Task.Run(() =>
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    Console.WriteLine(" Method 1");
-                    // Do something
-                    Task.Delay(100).Wait();
-                }
-            });
-        }
+   public static async Task callMethod()
+   {
+     Method2();
+     var count = await Method1();
+     Method3(count);
+   }
 
+   public static async Task<int> Method1()
+   {
+     int count = 0;
+     await Task.Run(() =>
+     {
+       for (int i = 0; i < 100; i++)
+       {
+         Console.WriteLine(" Method 1");
+         count += 1;
+       }
+     });
+     return count;
+   }
 
-        public static void Method2()
-        {
-            for (int i = 0; i < 25; i++)
-            {
-                Console.WriteLine(" Method 2");
-                // Do something
-                Task.Delay(100).Wait();
-            }
-        }
-    }
+   public static void Method2()
+   {
+     for (int i = 0; i < 25; i++)
+     {
+       Console.WriteLine(" Method 2");
+     }
+   }
+
+   public static void Method3(int count)
+   {
+     Console.WriteLine("Total count is " + count);
+   }
+}
 }
