e0889eb6db28727f5f3bd745b6504bdec573f2d0
Si Lam <silam@hotmail.com>
2022-03-25 14:37:46 -0500

    Add git ignore

---
+++ BulkyBook/BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        // dataannotation 
+        // primary key, identity column
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+
+    }
+}
---
+++ BulkyBook/BulkyBook/BulkyBookWeb/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// you can add database or email services right here into container
+
+builder.Services.AddControllersWithViews();
+
+// auto reload in NET 6 , so no comper
+// builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
